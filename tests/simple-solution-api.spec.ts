8f86e1d5e40577fa9a913e53e149af8811221c1e
Jevgeni <jevgeni.massalov@gmail.com>
2025-06-09 20:00:50 +0300

    Added new tests

--- tests/simple-solution-api.spec.ts
+++ tests/simple-solution-api.spec.ts
@@ -31,3 +31,36 @@ test('post order with correct data should receive code 201', async ({ request })
   console.log('response body:', await response.json())
   expect(response.status()).toBe(StatusCodes.OK)
 })
+
+test('get order with orderId should 0 receive code 400', async ({ request }) => {
+  // Build and send a GET request to the server
+  const response = await request.get('https://backend.tallinn-learning.ee/test-orders/0')
+  expect(response.status()).toBe(StatusCodes.BAD_REQUEST)
+})
+test('get order with orderId 11 should receive code 400', async ({ request }) => {
+  // Build and send a GET request to the server
+  const response = await request.get('https://backend.tallinn-learning.ee/test-orders/11')
+  expect(response.status()).toBe(StatusCodes.BAD_REQUEST)
+})
+test('get order with orderId NULL should receive code 500', async ({ request }) => {
+  // Build and send a GET request to the server
+  const response = await request.get('https://backend.tallinn-learning.ee/test-orders/')
+  expect(response.status()).toBe(StatusCodes.INTERNAL_SERVER_ERROR)
+})
+test('get order with orderId test should receive code 400', async ({ request }) => {
+  // Build and send a GET request to the server
+  const response = await request.get('https://backend.tallinn-learning.ee/test-orders/test')
+  expect(response.status()).toBe(StatusCodes.BAD_REQUEST)
+})
+test('post order with correct data should receive code 415', async ({ request }) => {
+  // prepare request body
+
+  // Send a POST request to the server
+  const response = await request.post('https://backend.tallinn-learning.ee/test-orders', {
+    data: 'test',
+  })
+  // Log the response status and body
+  console.log('response status:', response.status())
+  console.log('response body:', await response.json())
+  expect(response.status()).toBe(StatusCodes.UNSUPPORTED_MEDIA_TYPE)
+})
