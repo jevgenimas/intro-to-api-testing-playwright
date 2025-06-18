02ccc9ce2a24450da2600b9ddcd9a5777cd289a6
Jevgeni <jevgeni.massalov@gmail.com>
2025-06-18 18:09:40 +0300

    Added new tests for lesson 11

--- tests/simple-solution-api.spec.ts
+++ tests/simple-solution-api.spec.ts
@@ -1,6 +1,7 @@
 import { expect, test } from '@playwright/test'
 
 import { StatusCodes } from 'http-status-codes'
+import { OrderDto } from './dto/order-dto'
 
 test('get order with correct id should receive code 200', async ({ request }) => {
   // Build and send a GET request to the server
@@ -9,27 +10,20 @@ test('get order with correct id should receive code 200', async ({ request }) =>
   console.log('response body:', await response.json())
   console.log('response headers:', response.headers())
   // Check if the response status is 200
-  expect(response.status()).toBe(200)
+  expect.soft(response.status()).toBe(200)
 })
 
 test('post order with correct data should receive code 201', async ({ request }) => {
   // prepare request body
-  const requestBody = {
-    status: 'OPEN',
-    courierId: 0,
-    customerName: 'string',
-    customerPhone: 'string',
-    comment: 'string',
-    id: 0,
-  }
+  const requestBody = OrderDto.createOrderWithRandomData();
   // Send a POST request to the server
   const response = await request.post('https://backend.tallinn-learning.ee/test-orders', {
     data: requestBody,
   })
   // Log the response status and body
   console.log('response status:', response.status())
-  console.log('response body:', await response.json())
-  expect(response.status()).toBe(StatusCodes.OK)
+  console.log('request body:', requestBody)
+  expect.soft(response.status()).toBe(StatusCodes.OK)
 })
 
 test('get order with orderId should 0 receive code 400', async ({ request }) => {
